5d98732a37630dcbf186329bfd6ba3c12ef1d4d0
XUANDANG1109 <dangthilinhxuan@gmail.com>
2022-08-22 14:29:17 +0300

    rrrr

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ print("good")
 print("morning")
 user = input('Enter your name:')
 print(user)
+first_number = 3
+print(first_number)
+fahrenheit_str = input('Give me a number:')
+fahrenheit = float(fahrenheit_str)
+celsius = (fahrenheit - 32)*5/9
+print("The temperature in Celsius is: ", str(celsius))
+print(f"The temperature in Celsius: {celsius:6.2f}")
+
+
